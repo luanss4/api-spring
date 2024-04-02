4ab3d1ddce0e3f759da4ecaafd23bde3d58dbb30
C1293361 Luan Sousa Santana <c1293361@interno.bb.com.br>
2024-04-01 23:01:12 -0300

    arquivos iniciais

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,13 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosMedico;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @PostMapping
+    public void cadastrar(@RequestBody DadosMedico medico){
+        System.out.println(medico);
+    }
+}
